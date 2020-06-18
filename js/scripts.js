bb224fa86a3914c833f3f4898de160f549b8c111
k <kkabubenson@gmail.com>
2020-06-18 06:07:29 -0400

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#heading1").click(function(){
+      $("#hidden1").slideToggle();
+    });
+    $("#heading2").click(function(){
+      $("#hidden2").slideToggle();
+    });
+    $("#heading3").click(function(){
+      $("#hidden3").slideToggle();;
+    });
+    $("#heading4").click(function(){
+      $("#hidden4").slideToggle();;
+    });
+    $("#heading5").click(function(){
+      $("#hidden5").slideToggle();;
+      });
+    $("#heading6").click(function(){
+      $("#hidden6").slideToggle();;
+      });
+  });
